43c361b993375263bd78f106ab7a8e2dd28e939a
razvan goga <razvan.goga@gmail.com>
2012-10-30 14:12:16 +0200

    cookies using bottle

--- bottle_.py
+++ bottle_.py
@@ -5,13 +5,13 @@ import pymongo
 @bottle.route('/')
 def home_page():
 	fruits = ['apple', 'orange', 'grape', 'peach', 'banana']
-	return bottle.template('bottle_01', username="rg", model=fruits);
+	return bottle.template('bottle_index', username="rg", model=fruits);
 	
 #web server alternate route
 @bottle.route('/2')
 def home_page():
 	fruits = ['apple', 'orange', 'grape', 'peach', 'banana']
-	return bottle.template('bottle',  { "username":"rg", "model":fruits });
+	return bottle.template('bottle_index',  { "username":"rg", "model":fruits });
 
 #web server post favourite fruit route	
 @bottle.post('/favourite_fruit')
@@ -21,7 +21,14 @@ def favourite_fruit():
 	if(fruit == None or fruit == ""):
 		fruit = "No fruit selected"
 	
-	return bottle.template("bottle_fruit_selected", {"fruit": fruit })
+	bottle.response.set_cookie("fruit", fruit)
+	bottle.redirect("/show_fruit")
+
+#web server show fruit route
+@bottle.route('/show_fruit')
+def show_fruit():
+	fruit = bottle.request.get_cookie("fruit")
+	return bottle.template('bottle_fruit_selected',  { "fruit":fruit });
 	
 #added to make the web server watch the files for chages (and not need restart)
 bottle.debug(True)
