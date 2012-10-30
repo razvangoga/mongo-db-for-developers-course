f3fc809b25e32721425b53410bae7187cb74f599
Razvan Goga <razvan.goga@gmail.com>
2012-10-30 13:58:28 +0200

    added post action

--- bottle_01.py
+++ bottle_01.py
@@ -11,7 +11,17 @@ def home_page():
 @bottle.route('/2')
 def home_page():
 	fruits = ['apple', 'orange', 'grape', 'peach', 'banana']
-	return bottle.template('bottle_01',  { "username":"rg", "model":fruits });	
+	return bottle.template('bottle_01',  { "username":"rg", "model":fruits });
+
+#web server post favourite fruit route	
+@bottle.post('/favourite_fruit')
+def favourite_fruit():
+	fruit = bottle.request.forms.get("fruit");
+	
+	if(fruit == None or fruit == ""):
+		fruit = "No fruit selected"
+	
+	return bottle.template("bottle_02_fruit_selected", {"fruit": fruit })
 	
 #added to make the web server watch the files for chages (and not need restart)
 bottle.debug(True)
---
+++ bottle_02_fruit_selected.tpl
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello World!</title>
+	</head>
+	<body>
+		<h3>Your favourite fruit is {{fruit}}</h3>
+	</body>
+</html>
