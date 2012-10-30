c96d6cc5ecce272dbe4c299399fe35864f886cc5
razvan goga <razvan.goga@gmail.com>
2012-10-30 13:17:27 +0200

    more getting started with dictionaries & lists

--- python_getting_started_02.py
+++ python_getting_started_02.py
@@ -1,4 +1,9 @@
-person = {'name':'RG', 'color':'red', 'hair':'brown'}
+person = {'name':'RG', 'color':'red', 'hair':'brown', 'interests' : ['cycling','running','biking'] }
 
 for key in person:
-	print "key is " + key + " value is " + person[key]
+	if (key == 'interests'):
+		print "Interests...."
+		for interest in person[key]:
+			print "\tinterest is " + interest
+	else:
+		print "key is " + key + " value is " + person[key]
